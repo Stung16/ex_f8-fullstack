047b41075935ed7e8d2acd11cafa026159e807f6
Stung16 <kieuduytung3@gmail.com>
2023-08-26 18:17:12 +0700

    update

--- Day23/main.js
+++ Day23/main.js
@@ -7,16 +7,24 @@ var tab_login =document.querySelector('.tab-login')
 var title_header=document.querySelector('.title-header')
 var box_login=document.querySelector('.box-login')
 var box_register=document.querySelector('.box-register')
-var text_input =document.querySelector('.text-input')
-var title =document.querySelector('.title')
+var text_input =document.getElementsByClassName('text-input')
+var title =document.getElementsByClassName('title')
+var form_login =document.querySelector('.form-login')
+var form_register =document.querySelector('.form-register')
+var err_email_login =document.querySelector('.err_email-login')
+var err_pass_login =document.querySelector('.err_pass-login')
+var err_name_register =document.querySelector('.err_name-register')
+var err_email_register =document.querySelector('.err_email-register')
+var err_pass_register =document.querySelector('.err_pass-register')
+var email_login = document.getElementById('email_login')
+var pass_login = document.getElementById('pass_login')
+var name_register = document.getElementById('name_register')
+var email_register = document.getElementById('email_register')
+var pass_register = document.getElementById('pass_register')
+var typePasswordLogin = document.querySelector(".form-login input[type='password']")
+var typePasswordRegister = document.querySelector(".form-register input[type='password']")
+var showHideLogin =document.querySelector('.eye i')
 
-
-var text_onblur = text_input.onblur = function(){
-    title.innerHTML =`<p class ="warring_title ">Vui lòng nhập thông tin</p>`
-}
-var text_focus = text_input.onfocus = function(){
-    title.innerHTML ='';
-}
 
 btn.addEventListener('click',function(){
     form_user.classList.add('active')
@@ -32,6 +40,8 @@ overlay.addEventListener('click',function(){
     form_user.classList.remove('active')
     box_register.classList.remove('active')
     box_login.classList.remove('active')
+    resetFormLogin()
+    resetFormRegister()
 
 })
 tab_login.addEventListener('click',function(){
@@ -39,18 +49,143 @@ tab_login.addEventListener('click',function(){
     tab_register.classList.remove('title-active')
     box_login.classList.add('active')
     box_register.classList.remove('active')
-    if(tab_login.className ==='title-active'){
-        if(text_focus){
-            text_onblur();
-        }
+    resetFormRegister()
 
-    }
 })
 tab_register.addEventListener('click',function(){
     tab_register.classList.add('title-active')
     tab_login.classList.remove('title-active')
     box_register.classList.add('active')
     box_login.classList.remove('active')
+    resetFormLogin()
 })
 
+function validateEmail (mail,ele){
+    if(mail.value===''){
+        ele.innerHTML = "Vui lòng nhập thông tin"
+    }else {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+      {
+        ele.innerHTML = ""
+      } else {
+        ele.innerHTML = "Vui lòng nhập đúng định dạng email"
+    }
+    }
+}
+function validatePass (pass,ele){
+    if(pass.value===''){
+        ele.innerHTML = 'Vui lòng nhập thông tin'
+    }else{
+        if(pass.value.length >= 6 && pass.value.length <20){
+            ele.innerHTML = ""
+        }else{
+            ele.innerHTML = 'mật khẩu ít nhất có 6-20 ký tự'
+        }
+    }
+}
 
+
+function ValidateName(name,ele) {
+    if(name.value === "") {
+      ele.innerHTML = "Vui lòng nhập thông tin"
+    }else {
+      ele.innerHTML = ""
+    }
+  }
+  function resetFormLogin() {
+    form_login.reset()
+    err_email_login.innerHTML = ""
+    err_pass_login.innerHTML = ""
+  }
+  
+  function resetFormRegister() {
+    form_register.reset()
+    err_name_register.innerHTML = ''
+    err_email_register.innerHTML = ''
+    err_pass_register.innerHTML =''
+  }
+
+
+//   showHideLogin.addEventListener('click',function() {
+//     if(typePasswordLogin.type === password){
+//         typePasswordLogin.type = 'text'
+//     }else{
+//         typePasswordLogin.type = 'password'
+
+//     }
+// })
+  
+//   showHideRegister.addEventListener('click',function() {
+//     showPass(this,typePasswordRegister)
+//   })
+
+
+
+
+
+
+// login
+  pass_login.addEventListener('input', function() {
+    validatePass(pass_login,err_pass_login)
+    validateEmail(email_login,err_email_login)
+    box_login.classList.add('active')
+  })
+  
+  email_login.addEventListener('input', function() {
+    validateEmail(email_login,err_email_login)
+    validatePass(pass_login,err_pass_login)
+  })
+
+  pass_login.addEventListener('blur', function() {
+    if(pass_login.value === "" && email_login.value === "") {
+        err_pass_login.innerHTML = "Vui lòng nhập thông tin"
+        err_email_login.innerHTML = "Vui lòng nhập thông tin"
+      }
+  })
+  email_login.addEventListener('blur', function() {
+    if(pass_login.value === "" && email_login.value === "") {
+        err_pass_login.innerHTML = "Vui lòng nhập thông tin"
+        err_email_login.innerHTML = "Vui lòng nhập thông tin"
+      }
+  })
+//   resgiter
+name_register.addEventListener('input', function() {
+    ValidateName(name_register,err_name_register)
+    validatePass(pass_register,err_pass_register)
+    validateEmail(email_register,err_email_register)
+  })
+
+pass_register.addEventListener('input', function() {
+    ValidateName(name_register,err_name_register)
+    validatePass(pass_register,err_pass_register)
+    validateEmail(email_register,err_email_register)
+  })
+  
+  email_register.addEventListener('input', function() {
+    ValidateName(name_register,err_name_register)
+    validateEmail(email_register,err_email_register)
+    validatePass(pass_register,err_pass_register)
+  })
+  name_register.addEventListener('blur', function() {
+    if(pass_register.value === "" && email_register.value === "" && name_register.value==="") {
+        err_pass_register.innerHTML = "Vui lòng nhập thông tin"
+        err_email_register.innerHTML = "Vui lòng nhập thông tin"
+        err_name_register.innerHTML = "Vui lòng nhập thông tin"
+      }
+  })
+  pass_register.addEventListener('blur', function() {
+    if(pass_register.value === "" && email_register.value === "" && name_register.value==="") {
+        err_pass_register.innerHTML = "Vui lòng nhập thông tin"
+        err_email_register.innerHTML = "Vui lòng nhập thông tin"
+        err_name_register.innerHTML = "Vui lòng nhập thông tin"
+
+      }
+  })
+  email_register.addEventListener('blur', function() {
+    if(pass_login.value === "" && email_login.value === "" && name_register.value==="") {
+        err_pass_login.innerHTML = "Vui lòng nhập thông tin"
+        err_email_login.innerHTML = "Vui lòng nhập thông tin"
+        err_name_register.innerHTML = "Vui lòng nhập thông tin"
+
+      }
+  })
