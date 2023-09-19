47832e8defbfe38c728d959797e11c5eb92dc8f8
Stung16 <kieuduytung3@gmail.com>
2023-09-20 05:24:48 +0700

    update

--- Day29/main.js
+++ Day29/main.js
@@ -1,150 +1,48 @@
-// var list = document.querySelector(".list")
-// var listItems = list.querySelectorAll(".list-item")
+var list = document.querySelector(".list")
+Array.from(list.children).forEach(function(item) {
+    item.draggable = "true";
+    item.addEventListener("dragstart", function () {
+        item.classList.add("opacity");
+      });
+      item.addEventListener("dragend", function () {
+        item.classList.remove("opacity");
+      });
+})
 
-// listItems.forEach(function(item,index) {
-//     item.addEventListener("dragstart", function() {
-//         item.classList.add("dragging")
-
-//     })
-//     item.addEventListener("dragend", function() {
-//         item.classList.remove("dragging")
-//     })
-//     // let itemIndex = 0
-//     // let moduleIdex = 0
-//     // let title ="Bài "
-//     // if(item.classList.contains("module")){
-//     //     title = "Module "
-//     //     moduleIdex++
-//     // }else{
-//     //     itemIndex++
-//     // }
-
-//     // if(!item.children.length){
-
-//     // }
-
-// })
-
-// var initSortList = function(e) {
-//     e.preventDefault()
-//     var draggingItem =list.querySelector(".dragging")
-//     var siblings =[...list.querySelectorAll(".list-item:not(.dragging)")]
-//     let nextSibling = siblings.find(function(sibling) {
-//         return e.clientY <= sibling.offsetTop + sibling.offsetHeight /2 + sibling.offsetHeight /4
-//     })
-//     list.insertBefore(draggingItem,nextSibling)
-// }
-// list.addEventListener("dragover",initSortList)
-
-var list = document.querySelector(".list");
-const listTable = [
-  {
-    module: "Nhập môn lập trình web",
-    lessons: [
-      "Giới thiệu Khóa học HTML-CSS",
-      "Nhập môn lập trình web - Phần 1",
-      "Nhập môn lập trình web - Phần 2",
-      "Nhập môn lập trình web - Phần 3",
-      "Nhập môn lập trình web - Phần 4",
-      "Nhập môn lập trình web - Phần 5",
-      "Nhập môn lập trình web - Phần 6",
-      "Nhập môn lập trình web - Phần 7",
-      "Nhập môn lập trình web - Phần 8",
-      "Nhập môn lập trình web - Phần 9",
-      "Nhập môn lập trình web - Phần 10",
-    ],
-  },
-  {
-    module: "Ngôn ngữ HTML",
-    lessons: [
-      "Công cụ - Phần mềm cần chuẩn bị",
-      "Ngôn ngữ HTML",
-      "Thuộc tính Float trong CSS",
-      "Nhập môn lập trình web - Phần 11",
-      "Nhập môn lập trình web - Phần 12",
-      "Nhập môn lập trình web - Phần 13",
-      "Nhập môn lập trình web - Phần 14",
-      "Nhập môn lập trình web - Phần 15",
-      "Nhập môn lập trình web - Phần 16",
-      "Nhập môn lập trình web - Phần 17",
-      "Nhập môn lập trình web - Phần 18",
-      "Nhập môn lập trình web - Phần 19",
-      "Nhập môn lập trình web - Phần 20",
-      "Nhập môn lập trình web - Phần 21",
-      "Nhập môn lập trình web - Phần 22",
-    ],
-  },
-  {
-    module: "Ngôn ngữ CSS",
-    lessons: [
-      "Thuộc tính Position trong CSS",
-      "Tích hợp font-awesome và kỹ thuật liên quan",
-      "Tạo bộ đếm (Counter) trong CSS",
-      "Nhập môn lập trình web - Phần 23",
-      "Nhập môn lập trình web - Phần 24",
-      "Nhập môn lập trình web - Phần 25",
-      "Nhập môn lập trình web - Phần 26",
-      "Nhập môn lập trình web - Phần 27",
-    ],
-  },
-];
-
-// listTable.forEach((topic, indexTopic) => {
-//     topic.lessons.forEach(function(item,index) {
-//         console.log(item,index);
-//     })
-// })
-const result = listTable
-  .map(
-    (list, index) =>
-      ` <div class="list-item module" draggable="true">
-        <span>Module ${index + 1}:</span>
-        ${list.module}
-      </div>
-      ${list.lessons
-        .map(
-          (item, index) =>
-            `<div class="list-item" draggable="true">
-        <span>Bài ${index + 1}:</span>
-          ${item}
-        </div>`
-        )
-        .join("")}`
-  )
-  .join("");
-list.innerHTML = result;
-var listItems = list.querySelectorAll(".list-item");
-listItems.forEach(function (item, index) {
-  item.addEventListener("dragstart", function () {
-    item.classList.add("dragging");
-  });
-  item.addEventListener("dragend", function () {
-    item.classList.remove("dragging");
-  });
-});
-var initSortList = function (e) {
-  e.preventDefault();
-  var draggingItem = list.querySelector(".dragging");
-  var siblings = [...list.querySelectorAll(".list-item:not(.dragging)")];
-  let nextSibling = siblings.find(function (sibling) {
-    return (
-      e.clientY <=
-      sibling.offsetTop + sibling.offsetHeight / 2 + sibling.offsetHeight / 4
-    );
-  });
-  list.insertBefore(draggingItem, nextSibling);
-};
-list.addEventListener("dragover", initSortList);
-
-list.addEventListener("dragend", function () {
-  const listItem = document.querySelectorAll(".list-item:not(.module) span");
-  const listModule = document.querySelectorAll(".list-item.module span");
-  listItem.forEach((item, index) => {
+var lessons = document.querySelectorAll(".list-item:not(.title-module)")
+lessons.forEach(function (item,index) {
+    item.style.display = "flex"
     index++
-    item.innerHTML = `Bài ${index}:`;
-  });
-  listModule.forEach((module, index) => {
+    var innerItem =item.innerText
+    item.innerHTML =`<span class = "index-lesson">Bài:${index}</span><div>${innerItem}</div>`
+})
+
+var modules = document.querySelectorAll(".title-module")
+modules.forEach(function (item,index) {
+    item.style.display = "flex"
     index++
-    module.innerHTML = `Module ${index}:`;
-  });
+    var innerItem =item.innerText
+    item.innerHTML =`<span class = "index-module">Bài:${index}</span><div>${innerItem}</div>`
+})
+
+list.addEventListener('dragover',function(e) {
+    const itemDrag = document.querySelector(".list-item.opacity");
+    const itemsNotDrag = document.querySelectorAll(".list-item:not(.opacity)");
+    const node = Array.from(itemsNotDrag).find((item) => {
+      return e.pageY <= item.offsetTop + itemDrag.offsetHeight/2 + itemDrag.offsetHeight/4;
+    });
+    list.insertBefore(itemDrag, node);
+})
+
+  list.addEventListener("dragend", function () {
+    const listItem = document.querySelectorAll(".list-item:not(.title-module) span");
+    const listModule = document.querySelectorAll(".title-module span");
+    listItem.forEach((item, index) => {
+        index++
+        item.innerHTML = `Bài ${index}:`;
+      });
+      listModule.forEach((module, index) => {
+        index++
+        module.innerHTML = `Module ${index}:`;
+      });
 });
