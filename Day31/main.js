8661fef8cd00015267ade6d6f5d838f960683ed1
Stung16 <kieuduytung3@gmail.com>
2023-09-26 05:11:27 +0700

    update

--- Day31/main.js
+++ Day31/main.js
@@ -1,13 +1,28 @@
 var counter = document.querySelector(".counter");
-counter.innerText = +"30";
-var i = 5;
+var btn = document.querySelector(".btn");
 
+let timer = 0;
+let count = 30;
+let isDownload = false;
 
-setInterval(function() {
-    if(i >0 ){
-        i--;
-        counter.innerText = i;
-    }else{
-        clearInterval()
-    }
-}, 500);
+const actionCounter = function(start) {
+  if (timer <= start) {
+    count--;
+    counter.innerText = count;
+    timer = start + 1000;
+  }
+  if (count === 0) {
+    isDownload = true;
+    btn.removeAttribute("disabled");
+    btn.addEventListener("click",function(){
+        if (isDownload) {
+          window.location.href='https://fullstack.edu.vn'
+        }
+      });
+  }
+  if (count > 0) {
+    window.requestAnimationFrame(actionCounter);
+  }
+};
+requestAnimationFrame(actionCounter)
+
