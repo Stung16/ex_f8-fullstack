2261ec743b3f62f1b8fe8a19210606aa99324bfd
Stung16 <kieuduytung3@gmail.com>
2023-10-28 01:29:06 +0700

    update

--- Day38/assets/js/main.js
+++ Day38/assets/js/main.js
@@ -26,9 +26,9 @@ const renderBlogs = async () => {
     .map((blog) => {
       const timeMing = blog.createdAt;
       const createdAt = new Date(timeMing);
-      const DayCreat = `${createdAt.getDate()} - ${
+      const DayCreat = `${createdAt.getDate()}-${
         createdAt.getMonth() + 1
-      } - ${createdAt.getFullYear()} `;
+      }-${createdAt.getFullYear()} `;
       const timeCreat = `${createdAt.getHours()}:${
         createdAt.getMinutes() < 10
           ? "0" + createdAt.getMinutes()
@@ -54,8 +54,24 @@ const renderBlogs = async () => {
         </div>`;
     })
     .join("");
-  blog_post.innerHTML = htmlBlogs;
+    if(blog_post){
+      blog_post.innerHTML = htmlBlogs;
+    }
 };
+
+
+// const postStatus = () => {
+//   const create_post = document.querySelector(".create-post")
+//   if(create_post) {
+//     const handleContent = create_post.querySelector(".content").value
+//     const handleTitle = create_post.querySelector(".title").value
+//     create_post.addEventListener("submit", (e) => {
+//       e.preventDefault()
+//       console.log(handleTitle);
+//       console.log(handleContent);
+//     })
+//   }
+// }
 
 const getProfile = async () => {
   const name_user = document.querySelector(".name-user");
@@ -74,6 +90,7 @@ const getProfile = async () => {
       if (response.status === 200) {
         avatar_user.innerText = `${infor.data.name.charAt(0).toUpperCase()}`;
         name_user.innerText = `${infor.data.name}`;
+        renderDatetimePicker()
       }
     }
   }
@@ -202,68 +219,66 @@ const render = () => {
   </div>
   <div class="main-blogs">
     <div class="blog-post">
-    <div class="post">
-    <div class="inner-post">
-      <div class="timming">12:00</div>
-      <div class="infor-post">
-        <div class="infor-user-post">
-          <div class="avatar-user">T</div>
-          <div class="name-user">Kiều Tùng</div>
-        </div>
-        <div class="title-post">Hot Search</div>
-        <div class="content-post">Nhậu nhậu</div>
-      </div>
-    </div>
-    <hr />
-  </div>
+    
     </div>
   </div>`;
   }
   if (status) {
     container.innerHTML = `
     <div id="ui-login">
-    <div class="header-blog">
-      <h1>bloger</h1>
-      <div class="infor-user">
-        <div class="infor-user-post">
-          <div class="avatar-user"></div>
-          <div class="name-user">loading ...</div>
+      <div class="header-blog">
+        <h1>bloger</h1>
+        <div class="infor-user">
+          <div class="infor-user-post">
+            <div class="avatar-user"></div>
+            <div class="name-user">loading ...</div>
+        </div>
       </div>
-    </div>
-    <div class="main-blogs">
-      <form class="create-post">
-        <label for="title">Enter your title</label>
-        <input id="title" type="text" placeholder="Please enter the title">
-        <label for="content">Enter your content</label>
-        <textarea id="content" cols="30" rows="10" placeholder="Content here"></textarea>
-        <label for="time">Set time to post</label>
-        <input id="time" type="datetime-local">
-        <button class="btn wirite">Wirite</button>
-      </form>
-      <div class="btn btn_sign-out">Sign-out</div>
-      <div class="blog-post">
-        <div class="post">
-          <div class="inner-post">
-            <div class="timming">12:00</div>
-            <div class="infor-post">
-              <div class="infor-user-post">
-                <div class="avatar-user">T</div>
-                <div class="name-user">Kiều Tùng</div>
+      <div class="main-blogs">
+        <form class="create-post">
+          <label for="title">Enter your title</label>
+          <input id="title" class="title" type="text" placeholder="Please enter the title">
+          <label for="content">Enter your content</label>
+          <textarea id="content" class="content" cols="30" rows="10" placeholder="Content here"></textarea>
+          <label for="time">Set time to post</label>
+          <input type="text" class="input-date" readonly="" value="27/10/2023">
+          <div class="datetime-picker">
+            <div class="wrapper">
+              <div class="header">
+                  <p class="current-date"></p>
+                  <div class="icons">
+                      <span id="prev" class="material-symbols-rounded">&lt</span>
+                      <span id="next" class="material-symbols-rounded">&gt</span>
+                  </div>
               </div>
-              <div class="title-post">Hot Search</div>
-              <div class="content-post">Nhậu nhậu</div>
+              <div class="calendar">
+                  <ul class="weeks">
+                      <li>Sun</li>
+                      <li>Mon</li>
+                      <li>Tue</li>
+                      <li>Wed</li>
+                      <li>Thu</li>
+                      <li>Fri</li>
+                      <li>Sat</li>
+                  </ul>
+                  <ul class="days"></ul>
+              </div>
             </div>
           </div>
-          <hr />
+          <button class="btn wirite">Wirite</button>
+        </form>
+        <div class="btn btn_sign-out">Sign-out</div>
+        <div class="blog-post">
         </div>
       </div>
     </div>
-    </div>`;
+    <div class="overlay"></div>`;
   }
   renderBlogs();
   getProfile();
   eventSign_in();
   eventLogout();
+  // postStatus()
 };
 
 render();
@@ -311,6 +326,14 @@ const handleSignin = async ({ email, password }) => {
     };
     localStorage.setItem("login", JSON.stringify(tokens));
     render(); //Render lại giao diện
+
+    renderDatetimePicker()
+    
+
+
+
+
+
   } else {
     msg.innerText = `Information incorect`;
   }
@@ -358,3 +381,191 @@ const requestRefresh = async () => {
   }
   // render()
 };
+
+
+
+
+const renderDatetimePicker = () => {
+  const daysTag = document.querySelector(".days"),
+  currentDate = document.querySelector(".current-date"),
+  prevNextIcon = document.querySelectorAll(".icons span");
+  const input_date = document.querySelector(".input-date")
+  // getting new date, current year and month
+  let date = new Date(),
+  currYear = date.getFullYear(),
+  currMonth = date.getMonth();
+  
+  
+
+  // const btn_wiritePost = document.querySelector(".wirite")
+  const datetimePicker = document.querySelector(".datetime-picker")
+  const overlay = document.querySelector(".overlay")
+
+
+  if (input_date) {
+    input_date.addEventListener("click",() => {
+      datetimePicker.style.display= "block"
+      if(overlay){
+        overlay.style.display= "block"
+      }
+    })
+  }
+  if(overlay){
+    overlay.addEventListener("click",() => {
+      overlay.style.display= "none"
+      datetimePicker.style.display= "none"
+    })
+  }
+
+  
+  const getValue = () => {
+      const li = daysTag.querySelectorAll("li")
+      const overlay = document.querySelector(".overlay")
+      const datetimePicker = document.querySelector(".datetime-picker")
+
+      li.forEach(function (item){
+          item.addEventListener("click",function(){
+              if(daysTag.querySelector(".active")){
+                  daysTag.querySelector(".active").classList.remove("active");
+              }
+              this.classList.add("active")
+              input_date.value = `${currYear}-${currMonth+1}-${this.innerText}`
+              overlay.style.display= "none"
+              datetimePicker.style.display= "none"
+          })
+      })
+  }
+  
+  
+  
+  
+  
+  
+  // storing full name of all months in array
+  const months = ["January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December"];
+  const renderCalendar = () => {
+      let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+      lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+      lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+      lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+      let liTag = "";
+      for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+          liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+      }
+      for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+          // adding active class to li if the current day, month, and year matched
+          let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                       && currYear === new Date().getFullYear() ? "active" : "";
+          liTag += `<li class="${isToday}">${i}</li>`;
+      }
+      for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+          liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+      }
+      currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+      daysTag.innerHTML = liTag;
+      getValue()
+  }
+  renderCalendar();
+  prevNextIcon.forEach(icon => { // getting prev and next icons
+      icon.addEventListener("click", () => { // adding click event on both icons
+          // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+          currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+          if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+              // creating a new date of current year & month and pass it as date value
+              date = new Date(currYear, currMonth, new Date().getDate());
+              currYear = date.getFullYear(); // updating current year with new date year
+              currMonth = date.getMonth(); // updating current month with new date month
+          } else {
+              date = new Date(); // pass the current date as date value
+          }
+          renderCalendar(); // calling renderCalendar function
+      });
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const daysTag = document.querySelector(".days"),
+// currentDate = document.querySelector(".current-date"),
+// prevNextIcon = document.querySelectorAll(".icons span");
+// // getting new date, current year and month
+// let date = new Date(),
+// currYear = date.getFullYear(),
+// currMonth = date.getMonth();
+// // storing full name of all months in array
+// const months = ["January", "February", "March", "April", "May", "June", "July",
+//               "August", "September", "October", "November", "December"];
+// const renderCalendar = () => {
+//     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+//     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+//     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+//     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+//     let liTag = "";
+//     for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+//         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+//     }
+//     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+//         // adding active class to li if the current day, month, and year matched
+//         let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+//                      && currYear === new Date().getFullYear() ? "active" : "";
+//         liTag += `<li class="${isToday}">${i}</li>`;
+//     }
+//     for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+//         liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+//     }
+//     currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+//     daysTag.innerHTML = liTag;
+// }
+// renderCalendar();
+// prevNextIcon.forEach(icon => { // getting prev and next icons
+//     icon.addEventListener("click", () => { // adding click event on both icons
+//         // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+//         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+//         if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+//             // creating a new date of current year & month and pass it as date value
+//             date = new Date(currYear, currMonth, new Date().getDate());
+//             currYear = date.getFullYear(); // updating current year with new date year
+//             currMonth = date.getMonth(); // updating current month with new date month
+//         } else {
+//             date = new Date(); // pass the current date as date value
+//         }
+//         renderCalendar(); // calling renderCalendar function
+//     });
+// });
+
+
+// const hideDatetimePicker = () => {
+//   const overlay = document.querySelector(".overlay")
+//   const tableDateTime = document.querySelector(".datetime-picker")
+//   if(overlay){
+//     overlay.addEventListener("click",(e) => {
+//       e.preventDefault()
+//       overlay.style.display= "none"
+//       console.log(tableDateTime);
+//     })
+//   }
+// }
+
+// const showDatetimePicker = () => {
+
+// }
+// hideDatetimePicker()
+// showDatetimePicker()
