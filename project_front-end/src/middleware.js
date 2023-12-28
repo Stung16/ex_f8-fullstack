39eeae820743b4bb82801e2da2e0a3d912d929f9
Stung16 <kieuduytung3@gmail.com>
2023-12-28 15:15:19 +0700

    update

--- project_front-end/src/middleware.js
+++ project_front-end/src/middleware.js
@@ -1,21 +1,13 @@
 import { NextResponse } from "next/server";
-import { getSession } from "@auth0/nextjs-auth0/edge";
 
 // This function can be marked `async` if using `await` inside
 export const middleware = async (request) => {
-  const path = request.nextUrl.pathname;
-  // console.log(path);
   // const id = path.replace("/my-mindmap","")
   // console.log(id);
   // console.log(path);
-  let mode = "private";
-  const user = await getSession();
   // console.log(user);
-  if (!user) {
 
     // return NextResponse.redirect(new URL("/api/auth/login", request.url));
-  }
-  // console.log(request);
   //   return NextResponse.redirect(new URL('/home', request.url))
 };
 
