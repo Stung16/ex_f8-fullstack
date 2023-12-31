40d0ab90b82010d081867e2d977f27c7fc8790bf
Stung16 <kieuduytung3@gmail.com>
2024-01-01 03:13:05 +0700

    update

--- project_front-end/src/app/(mindmap)/my-mindmap/items/Board_share.js
+++ project_front-end/src/app/(mindmap)/my-mindmap/items/Board_share.js
@@ -1,6 +1,6 @@
 "use client";
 import { saveMindmap } from "@/services/mindmapServices";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 
 const Board_share = ({ data, setShow, id }) => {
